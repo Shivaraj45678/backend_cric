8e8adab6a06e45b9668120acfc9d14c7d23749b7
SSR0987 <shivaraj2r1@example.com>
2024-08-03 15:44:32 +0530

    new

--- cricket.js
+++ cricket.js
@@ -1,34 +1,43 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Users =require('./models/users.model')
+const Users = require('./models/users.model');
 const cors = require('cors'); 
-const app=express();
+
+const app = express();
 app.use(express.json());
 app.use(cors());
-app.listen(3001,()=>{
-    console.log("running on loacalhost 3001")
-})
 
-app.post('/addUser',async(req,res)=>{
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Running on localhost ${PORT}`);
+});
+
+app.post('/addUser', async (req, res) => {
     try {
-        const users =await Users.create(req.body);
+        const users = await Users.create(req.body);
         res.status(200).json(users);
     } catch (error) {
-        res.status(500).json({message:error.message})
+        res.status(500).json({ message: error.message });
     }
-})
+});
 
-app.get('/getAllUsers',async(req,res)=>{
+app.get('/getAllUsers', async (req, res) => {
     try {
         const users = await Users.find({});
         res.status(200).json(users);
     } catch (error) {
-        res.status(500).json({message:error.message})
+        res.status(500).json({ message: error.message });
     }
-})
+});
 
-mongoose.connect('mongodb+srv://shivaraju:172r1a0103@backendapi.g565pbc.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=BackendApi')
-    .then(() => console.log('Connected!'))
-    .catch(() => {
-        console.log("Connection failed");
-    });
+const connectionString = 'mongodb+srv://shivaraju:172r1a0103@backendapi.g565pbc.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=BackendApi';
+
+mongoose.connect(connectionString, {
+    serverSelectionTimeoutMS: 30000, // 30 seconds
+    socketTimeoutMS: 45000, // 45 seconds
+})
+.then(() => console.log('Connected to MongoDB!'))
+.catch((error) => {
+    console.error('Connection failed:', error.message);
+});
